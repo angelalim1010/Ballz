2dbbb9fea4932bc5b34083fc513bdc39ce26aaec
Angela Lim <angela.lim57@myhunter.cuny.edu>
2019-01-13 10:29:05 -0500

    fixing brick and main

--- js/ball.js
+++ js/ball.js
@@ -1,43 +1,43 @@
-// Represents one ball in the game.
+// Class for one ball in the game
 class Ball {
+	//Constructor
+	constructor(canvas) {
+		this.canvas = canvas;
+		this.RADIUS = 5; //RADIUS (convention: CAPITALCASE = variable you won't change
 
-  constructor(canvas) {
-    this.canvas = canvas;
-    this.RADIUS = 6;
+		this.position = canvas.getStartPosition(); //Initialize position variable
+		this.movement = new Movement(0, 0); //Initialize movement variable
 
-    this.position = canvas.getInitialPosition();
-    this.movement = new Movement(0, 0);
-  }
+		//this.dx = Math.cos(angle);
+		//this.dy = Math.sin(angle);
+	}
 
-  // Returns whether the ball is currently moving.
-  // All balls do not necesserily move at the beginning of the game.
-  isMoving() { return this.movement.isMoving() }
+	// Returns whether the ball is currently moving.
+	// All balls do not necesserily move at the beginning of the game.
+	isMoving() { return this.movement.isMoving() }
 
-  // Returns a new BallMove, based on the ball current position, speed and
-  // direction.
-  createMove() { return new BallMove(this.position, this.movement); }
+	//checkMove - checks movement of ball
+	//If ball has hit a tile, updates tile and bounces off in opposite direction
+	//If ball has hit a wall, bounces off in opposite direction
+	createMovement() {
+		return new BallMovement(this.position, this.movement);
+	}
 
-  // Draws the ball at its current location.
-  draw() {
-    this.canvas.setColor("#333333" /* red */);
-    this.canvas.draw().beginPath();
-    this.canvas.draw().arc(this.position.getX(), this.position.getY(), this.RADIUS,
-                           /*startAngle=*/0, /*endAngle=*/Math.PI * 2)
-    this.canvas.draw().fill();
-  }
+	//draw - Draws a ball using its current position
+	draw() {
+		this.canvas.setColor("#DDDDDD"); //Color = black
+		this.canvas.draw().beginPath();
+		this.canvas.draw().arc(this.position.getX(), this.position.getY(), this.RADIUS, 0, 2 * Math.PI);
+		this.canvas.draw().fill();
+		console.log('DRAWING BALL');
+	}
 
-  // Erases the area around the ball current position.
-  erase() {
-    const buffer = 5;
-    this.canvas.draw().clearRect(this.position.getX() - this.RADIUS - buffer,
-                                 this.position.getY() - this.RADIUS - buffer,
-                                 this.RADIUS * 2 + buffer,
-                                 this.RADIUS * 2 + buffer);
-  }
+	//move - Moves ball by updating xcor and ycor with dx and dy
+	//If ball has hit a wall, bounces off in opposite direction
+	move(movement) {
+		this.movement = movement;
+		this.position.move(movement); //position.x += dx; position.y += dy;
+		//console.log('X-pos:', this.position.getX(), ' Y-pos:', this.position.getY());
 
-  // Moves the ball using a given speed/direction.
-  move(movement) {
-    this.movement = movement;
-    this.position.move(movement);
-  }
+	}
 }
