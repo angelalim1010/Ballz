39a30b218915f5e3cc5db1063a4f14b4c7233362
Angela Lim <angela.lim57@myhunter.cuny.edu>
2019-01-13 20:42:28 -0500

    fixing canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -1,25 +1,23 @@
 // Represents the area where the game is played.
 class Canvas {
 
-  constructor() {
-    var jQueryCanvas = $('canvas');
-    this.canvas = jQueryCanvas[0].getContext('2d');
-    this.width = jQueryCanvas.width();
-    this.height = jQueryCanvas.height();
-    console.log('Canvas dimensions are ' + this.width + ' x ' + this.height);
-  }
+	constructor() {
+		var jQueryCanvas = $('canvas');
+		this.canvas = jQueryCanvas[0].getContext('2d');
+		this.width = jQueryCanvas.width();
+		this.height = jQueryCanvas.height();
+		this.startPosition = new Position (this.width / 2, this.height - 20);
+		console.log('Canvas dimensions are ' + this.width + ' x ' + this.height);
+	}
 
-  draw() { return this.canvas; }
+	// Accessors
+	getWidth() { return this.width; } //Return width of canvas (x)
+	getHeight() { return this.height; } //Return height of canvas (y)
+	getStartPosition() { return this.startPosition; } // Return the start position of the balls (width / 2, height - 20)
 
-  setColor(color) { this.canvas.fillStyle = color; }
-
-  // Return the dimension of the canvas.
-  getWidth() { return this.width; }
-  getHeight() { return this.height; }
-
-  // Return the initial position of the balls.
-  getInitialPosition() {
-    return new Position(this.width / 2, this.height - 10);
-  }
+	//Draws canvas
+	draw() { return this.canvas; }
+	//Sets color of object calling this function
+	setColor(color) { this.canvas.fillStyle = color; }
 
 }
