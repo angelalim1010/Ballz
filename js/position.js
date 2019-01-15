01ace31532fcde9f78671cdf66cf7842bfdc8a4f
Angela Lim <angela.lim57@myhunter.cuny.edu>
2019-01-14 22:30:27 -0500

    changing files

--- js/position.js
+++ js/position.js
@@ -1,18 +1,19 @@
-// Represents a position on the board, measured from top-left corner.
-class Position {
+class Position{
 
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
 
-	// Accessors
-	getX() { return this.x; }
-	getY() { return this.y; }
+    getX(){ return this.x; };
+    getY(){ return this.y; };
 
-	// Moves the position by a given delta (represented by class movement)
-	move(movement) {
-		this.x += movement.getDx();
-		this.y += movement.getDy();
-	}
+    setX(x){ this.x = x; }
+    setY(y){ this.y = y; }
+
+    move(movement){
+        this.x += movement.getDx();
+        this.y += movement.getDy();
+    }
+
 }
